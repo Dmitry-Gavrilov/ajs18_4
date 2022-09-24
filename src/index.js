c4e73e6d8739c41ab02861e0c6656e1015b22316
Dmitry-Gavrilov <91532052+Dmitry-Gavrilov@users.noreply.github.com>
2022-09-24 20:07:51 +1000

    pure function

--- src/index.js
+++ src/index.js
@@ -1,10 +1,2 @@
-function info() {
-  const m = new Map();
-  m.set('name', 'ajs');
-  m.set('version', '1.0.0');
-  return m;
-}
-
-export default {
-  info
-};
+import './css/style.css';
+import './js/app';
